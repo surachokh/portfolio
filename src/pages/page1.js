22e5b37eed8fb65cf786c55f12b7110c83a03a3e
Independent <38752974+surachokh@users.noreply.github.com>
2022-01-26 18:49:06 +0700

    Add: Page 1

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -7,13 +7,38 @@ export default class Page1 extends Component {
       <Box
         sx={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           height: "100vh",
-          alignItems: 'center',
-          backgroundColor: '#000123'
+          alignItems: "space-around",
+          backgroundColor: "#000123",
         }}
       >
-        <Typography sx={{color: 'white'}}>Page 1</Typography>
+        <Box sx={{ display: "flex", flexGrow: 0.5, alignItems: "center" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              paddingLeft: 20,
+            }}
+          >
+            <Typography sx={{ color: "white", fontSize: 64 }}>
+              Portfolio
+            </Typography>
+            <Typography sx={{ color: "white", fontSize: 96 }}>
+              Software
+            </Typography>
+            <Typography sx={{ color: "white", fontSize: 96 }}>
+              Engineer
+            </Typography>
+          </Box>
+        </Box>
+        <Box sx={{ display: "flex", flexGrow: 0.5, alignItems: "center" }}>
+            <Box sx={{display: 'flex', flexGrow: 1, justifyContent: 'end', paddingRight: 20}}>
+            <Typography sx={{ color: "white", fontSize: 96  }}>Surachok Hemadhulin</Typography>
+            </Box>
+        </Box>
       </Box>
     );
   }
