14a365b2cd4116c735140a38232aa1dca758afba
Independent <38752974+surachokh@users.noreply.github.com>
2022-01-26 19:20:47 +0700

    Add: Page 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { ThemeProvider } from "@mui/material";
-import { Box, createTheme } from "@mui/system";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { Component } from "react";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2";
--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,20 +1,49 @@
-import { Box, Typography } from '@mui/material';
-import React, { Component } from 'react';
+import { Box, Typography } from "@mui/material";
+import React, { Component } from "react";
+import profilePicture from '../0916 สุรโชค เหมะธุลิน (1).png'
 
 export default class Page2 extends Component {
   render() {
     return (
-        <Box
+      <Box
         sx={{
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "space-around",
           height: "100vh",
-          alignItems: 'center',
-          backgroundColor: '#000123'
+          alignItems: "center",
+          backgroundColor: "#000123",
         }}
       >
-        <Typography sx={{color: 'white'}}>Page 2</Typography>
+        <Box sx={{ display: "flex", flexGrow: 0.5, alignItems: "center" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              paddingLeft: 20,
+            }}
+          >
+            <img src={profilePicture} alt="Profile" style={{height: 450}}/>
+          </Box>
+        </Box>
+        <Box sx={{ display: "flex", flexGrow: 1, alignItems: "center" , maxWidth: '100vh'}}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: 'column',
+              flexGrow: 0.5,
+              justifyContent: "center",
+            }}
+          >
+            <Typography sx={{ color: "white", fontSize: 96 }}>
+              About me
+            </Typography>
+            <Typography sx={{ color: "white", fontSize: 24 }}>
+                I am software engineer that specialize on front end development. I graduated in Computer Engineering with 2.60 gpa. I am single and my military status is extempts. 
+            </Typography>
+          </Box>
+        </Box>
       </Box>
-    )
+    );
   }
 }
